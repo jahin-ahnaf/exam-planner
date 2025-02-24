f4ab0d620d2bd9eac85cc3b70f0da43840d12a06
JA Tech <jahinahnaf16@gmail.com>
2025-02-24 10:40:14 +0600

    Add AOS library and update Hero component for Study Planner landing page

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,44 +1,17 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+import Signup from "@/app/(auth-pages)/sign-up/page";
 
-export default function Header() {
+export default function Hero() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
+    <section className="flex flex-col items-center justify-center text-white px-6 mt-10">
+      <div className="text-center max-w-2xl bg-zinc-800 p-10 rounded-2xl shadow-lg">
+        <h1 className="text-4xl font-bold text-white">Study Planner</h1>
+        <p className="text-lg text-gray-300 mt-2">
+          The study management tool for students!
+        </p>
+        <button className="mt-6 bg-zinc-500 text-white px-6 py-3 rounded-xl shadow-md hover:bg-zinc-600 transition">
+          <a href="http://studyplannerbd.vercel.app/protected/">Get Started</a>
+        </button>
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-    </div>
+    </section>
   );
 }
